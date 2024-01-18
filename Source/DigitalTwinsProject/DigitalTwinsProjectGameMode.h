5cf75769c998c629e5efd7d7f1a6c1e314c6cc83
Ian Fleming <ianfleming678@gmail.com>
2024-01-17 20:08:46 -0800

    Initial commit for UE project

---
+++ Source/DigitalTwinsProject/DigitalTwinsProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DigitalTwinsProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADigitalTwinsProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADigitalTwinsProjectGameMode();
+};
+
+
+
