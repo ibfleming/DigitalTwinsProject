fd71de3e84957b9a8eba254b60712cf21736ca14
Ian Fleming <ianfleming678@gmail.com>
2024-01-19 18:43:10 -0800

    Added Dynamic Crosshair and Common UI Structure

--- Plugins/SocketIOClient/Intermediate/Build/Win64/x64/UnrealEditor/Development/SIOJEditorPlugin/Definitions.SIOJEditorPlugin.h
+++ Plugins/SocketIOClient/Intermediate/Build/Win64/x64/UnrealEditor/Development/SIOJEditorPlugin/Definitions.SIOJEditorPlugin.h
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-#include "C:/Users/lee4219/Documents/UnrealProjects_2024/Intermediate/Build/Win64/x64/DigitalTwinsProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-=======
-#include "C:/Users/flem8015/Documents/Unreal Projects/DigitalTwinsProject/Intermediate/Build/Win64/x64/DigitalTwinsProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
->>>>>>> Stashed changes
+#include "E:/Epic Games/Projects/DigitalTwinsProject/Intermediate/Build/Win64/x64/DigitalTwinsProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
